652db6452f933426bfc28094c703dd6bfa00015e
zhangsan <zhangsan@itcast.cn>
2023-08-16 18:00:53 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("回到2000");
         System.out.println("hello git!");
+        System.out.println("hello git4!");
     }
 }
